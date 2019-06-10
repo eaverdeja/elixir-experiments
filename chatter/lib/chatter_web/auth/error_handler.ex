d8d40fc1c4680426b68da4fd0d18b7ccd9cb7b77
Eduardo Verdeja <eaverdeja@gmail.com>
2019-06-10 07:46:03 -0300

    Redirecting back to the login page on auth* errors

--- chatter/lib/chatter_web/auth/error_handler.ex
+++ chatter/lib/chatter_web/auth/error_handler.ex
@@ -1,18 +1,15 @@
 defmodule ChatterWeb.Auth.ErrorHandler do
   import Plug.Conn
-
-  require Logger
+  import Phoenix.Controller, only: [put_flash: 3, redirect: 2]
+  import ChatterWeb.Router.Helpers, only: [session_path: 2]
 
   @behaviour Guardian.Plug.ErrorHandler
 
   @impl Guardian.Plug.ErrorHandler
-  def auth_error(conn, {type, _reason}, _opts) do
-    body = to_string(type)
-
-    Logger.info(body)
-
+  def auth_error(conn, {_type, _reason}, _opts) do
     conn
-    |> put_resp_content_type("text/plain")
-    |> send_resp(401, body)
+    |> put_flash(:error, "You must login to access this page!")
+    |> redirect(to: session_path(conn, :new))
+    |> halt()
   end
 end
